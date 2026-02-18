81c091357cad78a22341649b9534193f7e7cd776
hunadim <hmapulane@gmail.com>
2026-02-18 16:54:18 +0200

    chore: renamed step definition file for login

--- src/test/java/stepdefinitions/LoginStepDefinitions.java
+++ src/test/java/stepdefinitions/LoginStepDefinitions.java
@@ -4,7 +4,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class StepDefinitions {
+public class LoginStepDefinitions {
 
     @Given("the customer is on the login page")
     public void the_customer_is_on_the_login_page() {
@@ -15,7 +15,7 @@ public class StepDefinitions {
     @When("the customer logs in with username and password")
     public void the_customer_logs_in_with_username_and_password() {
         // Write code here that turns the phrase above into concrete actions
-        System.out.println("the customer logs in with username and password");;
+        System.out.println("the customer logs in with username and password");
     }
 
     @Then("the customer should be redirected to the home page")
