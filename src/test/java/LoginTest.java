ea35381d3477c6a6ed88ae629e878134b2c2de30
hunadim <hmapulane@gmail.com>
2026-02-19 21:50:08 +0200

    test: validate categories on shop page and dismiss password leakage alert and navigate to women fashion products

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,9 +1,17 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.pagefactory.ByChained;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class LoginTest {
 
@@ -22,7 +30,54 @@ public class LoginTest {
         driver.findElement(By.id("password")).sendKeys("demo");
         driver.findElement(By.id("loginBtn")).click();
 
-        driver.quit(); //end the session // recommended
+
+        //loading that happens when navigating to shop home page
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlContains("/shop.php"));
+
+
+        //then an alert dialog displays
+        final Map<String, Object> chromePrefs = new HashMap<>();
+        chromePrefs.put("credentials_enable_service", false);
+        chromePrefs.put("profile.password_manager_enabled", false);
+        chromePrefs.put("profile.password_manager_leak_detection", false); // <======== This is the important one
+
+        final ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setExperimentalOption("prefs", chromePrefs);
+
+
+        By categoryCard = new ByChained(By.className("offer-text"), By.className("mb-3")); //finding the category name
+
+
+        List<WebElement> allFourCards = driver.findElements(categoryCard); //finding all category names and storing in a List of webelements type variable
+        List<String> getCategories = allFourCards.stream().map(e -> e.getText().trim()).toList(); // this is to get the String for assertion purposes
+
+        List<String> expectedCategories = List.of(
+                "Men Fashion",
+                "Women Fashion",
+                "Kids Fashion",
+                "Electronics"
+        );
+
+        int numberOfCategories = allFourCards.size();
+        Assert.assertEquals(numberOfCategories, 4);
+        Assert.assertTrue(getCategories.containsAll(expectedCategories), "Categories mismatch"); //order match does not matter used containsall,
+
+        System.out.println("These are  the category names " + getCategories);
+
+        String categoryName = "Women Fashion";
+
+        for(WebElement deptCategory : allFourCards) {
+
+            String name = deptCategory.getText(); //findElement(getCategories).getText().trim();
+
+            if(name.equalsIgnoreCase(categoryName)) {
+
+                WebElement shopNowButton = driver.findElement(By.cssSelector("a.btn.btn-primary[href='womens-wear.php']"));
+                shopNowButton.click();
+            }
+        }
+
     }
 
 }
