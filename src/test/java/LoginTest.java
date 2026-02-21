6cb230627f968f601464873c30e8d869d0038c65
hunadim <hmapulane@gmail.com>
2026-02-21 16:38:41 +0200

    test: add setup method ChromeOptions, update variable names, add test to navigate to women fashion product catalogue

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -3,38 +3,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.pagefactory.ByChained;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class LoginTest {
 
-    public static void main(String[] args) {
+    WebDriver driver;
+    WebDriverWait wait;
 
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        driver.get("https://shop.qaautomationlabs.com/index.php"); //
-        driver.manage().window().maximize();
-        String title = driver.getTitle();
-        System.out.println(title); //QA AUTOMATIONLAB
-        Assert.assertEquals(title, "QA AUTOMATIONLAB");
-        System.out.println("Application is launched!");
-
-        driver.findElement(By.id("email")).sendKeys("demo@demo.com");
-        driver.findElement(By.id("password")).sendKeys("demo");
-        driver.findElement(By.id("loginBtn")).click();
-
-
-        //loading that happens when navigating to shop home page
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.urlContains("/shop.php"));
-
+    @BeforeMethod
+    public void testSetUp() {
 
         //then an alert dialog displays
         final Map<String, Object> chromePrefs = new HashMap<>();
@@ -45,39 +32,93 @@ public class LoginTest {
         final ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.setExperimentalOption("prefs", chromePrefs);
 
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
 
-        By categoryCard = new ByChained(By.className("offer-text"), By.className("mb-3")); //finding the category name
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+    }
+    @Test
+    public void validLoginAndVerifyShopDepartments() {
+
+        driver.get("https://shop.qaautomationlabs.com/index.php"); //
+        Assert.assertEquals(driver.getTitle(), "QA AUTOMATIONLAB", "Title name mismatch");
+
+        driver.findElement(By.id("email")).sendKeys("demo@demo.com");
+        driver.findElement(By.id("password")).sendKeys("demo");
+        driver.findElement(By.id("loginBtn")).click();
 
 
-        List<WebElement> allFourCards = driver.findElements(categoryCard); //finding all category names and storing in a List of webelements type variable
-        List<String> getCategories = allFourCards.stream().map(e -> e.getText().trim()).toList(); // this is to get the String for assertion purposes
+        //loading that happens when navigating to shop home page
+        wait.until(ExpectedConditions.urlContains("/shop.php"));
 
-        List<String> expectedCategories = List.of(
+
+        //test case: verify that all 4 category cards are displayed and validate the category names
+
+        //finding all category names and storing in a List of webElements type variable
+        //Using List to allow order of insertion and duplicates because we want all categories that are displayed
+
+//        List<WebElement> shopDepartments = driver.findElements(By.cssSelector("div.product-offer"));
+//        System.out.println("Shop departments details: " +shopDepartments);
+//
+//        List<String> getDepartmentNames = shopDepartments.stream().map(e -> e.getText().trim()).toList();
+//        // this is to get the String for assertion purposes
+//        System.out.println("Get departments names: " +getDepartmentNames);
+
+
+        List<WebElement> shopDepartmentList = driver.findElements(By.cssSelector("div.product-offer"));
+        List<String> actualShopDepartments = new ArrayList<>();
+
+        List<String> expectedShopDepartments = List.of(
                 "Men Fashion",
                 "Women Fashion",
                 "Kids Fashion",
                 "Electronics"
         );
 
-        int numberOfCategories = allFourCards.size();
-        Assert.assertEquals(numberOfCategories, 4);
-        Assert.assertTrue(getCategories.containsAll(expectedCategories), "Categories mismatch"); //order match does not matter used containsall,
 
-        System.out.println("These are  the category names " + getCategories);
+        for (WebElement name : shopDepartmentList) {
+            String getDepartmentName = name.findElement(By.cssSelector("div.offer-text > h3")).getText().trim();
+            actualShopDepartments.add(getDepartmentName);
 
-        String categoryName = "Women Fashion";
+        }
 
-        for(WebElement deptCategory : allFourCards) {
+        Assert.assertEqualsNoOrder(
+                actualShopDepartments.toArray(),
+                expectedShopDepartments.toArray(),
+                "Department names mismatch."
+        );
 
-            String name = deptCategory.getText(); //findElement(getCategories).getText().trim();
+        Assert.assertEquals(actualShopDepartments.size(), 4, "Count of departments mismatch"); // assert number of categories
+        Assert.assertTrue(actualShopDepartments.containsAll(expectedShopDepartments), "Categories mismatch"); //order match does not matter used containsall,
 
-            if(name.equalsIgnoreCase(categoryName)) {
+        driver.quit();
+    }
+    @Test
+    public void navigateToWomenFashion() {
+        driver.get("https://shop.qaautomationlabs.com/index.php"); //
+        Assert.assertEquals(driver.getTitle(), "QA AUTOMATIONLAB", "Title name mismatch");
 
-                WebElement shopNowButton = driver.findElement(By.cssSelector("a.btn.btn-primary[href='womens-wear.php']"));
+        driver.findElement(By.id("email")).sendKeys("demo@demo.com");
+        driver.findElement(By.id("password")).sendKeys("demo");
+        driver.findElement(By.id("loginBtn")).click();
+        wait.until(ExpectedConditions.urlContains("/shop.php"));
+
+        List<WebElement> shopDepartmentList = driver.findElements(By.cssSelector("div.product-offer"));
+        String targetCategory = "Women Fashion";
+
+
+        for (WebElement name : shopDepartmentList) {
+            String getDepartmentName = name.findElement(By.cssSelector("div.offer-text > h3")).getText().trim();
+            if(getDepartmentName.equalsIgnoreCase(targetCategory)) {
+                WebElement shopNowButton = driver.findElement(
+                        By.cssSelector("a.btn.btn-primary[href='womens-wear.php']"));
                 shopNowButton.click();
+                break;
             }
         }
 
+        driver.quit();
     }
 
 }
