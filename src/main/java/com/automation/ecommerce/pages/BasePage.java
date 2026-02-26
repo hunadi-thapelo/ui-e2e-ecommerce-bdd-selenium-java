fa820126a916c6130ac1b05e559d7a7e09c4c1af
hunadim <hmapulane@gmail.com>
2026-02-26 15:41:29 +0200

    chore: add reusable method to perform clear and type actions for input type fields

--- src/main/java/com/automation/ecommerce/pages/BasePage.java
+++ src/main/java/com/automation/ecommerce/pages/BasePage.java
@@ -1,14 +1,24 @@
-package pages;
+package com.automation.ecommerce.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
 
     protected WebDriver driver;
+    protected WebDriverWait wait;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
-        PageFactory.initElements(driver, this);
+        PageFactory.initElements(driver, this); //This call sets the WebElement fields
+    }
+
+    protected void clearAndType(WebElement element, String text) { //added here because this is a common WebDriver actions
+        wait.until(ExpectedConditions.visibilityOf(element));
+        element.clear();
+        element.sendKeys();
     }
 }
